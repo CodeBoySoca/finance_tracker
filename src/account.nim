109331596280f9bbd919b67f848b32153b9faa55
Rocky Latchman <kidmongoose@gmail.com>
2024-06-22 00:39:18 -0700

    createAccount procedure to add a user to the database added along with the user creation prompts

--- src/account.nim
+++ src/account.nim
@@ -1,12 +1,17 @@
-import std/strformat
 import nimcrypto
+import db_connector/db_sqlite
 
 
-proc createAccount*(username: string, password: string)=
-   let hashed_passwd = keccak_256.digest(password)
+
+proc createAccount*(username: string, fullname: string,  password: string, salary: int)=
+   let db = open("accounts.db", "", "", "")
+   db.exec(sql"INSERT INTO user (username, fullname, password, salary) VALUES (?, ?, ?, ?)", username, fullname, keccak_256.digest(password), salary)
+   db.close()
     
-proc getAccount*(username: string, password: string): string  =
-    discard
+# proc getAccount*(username: string, password: string): string  =
+#     for x in db.fastRows(sql"SELECT (user_id) FROM user WHERE username=? AND password=?", username, keccak_256.digest(password)):
+#         echo x
 
 proc cancelAccount*(username: string, password: string)=
     discard
+
--- src/finance_tracker.nim
+++ src/finance_tracker.nim
@@ -1,6 +1,7 @@
 import std/math
-import strutils
+import strutils, typetraits
 import std/strformat
+
 
 import account
 
@@ -16,10 +17,23 @@ echo "Enter your choice and press Enter"
 let optionResult = readLine(stdin).parseInt()
 
 if optionResult == 1:
-   #createAccount()
+    echo "Very good! Lets set up your account\n"
+    echo "First, lets get your full name:"
+    let fullname: string = readLine(stdin)
+    echo "What is your username? (this is what you will use when you log in):"
+    let username: string = readLine(stdin)
+    echo "Nice, now we will need your password:"
+    let passwd: string = readLine(stdin)
+    echo "Next, to help you manage your money, we will need to know your monthly salary after taxes(ex. 5000)\n"
+    echo "salary:$"
+    let salary: int = readLine(stdin).parseInt()
+    let formattedSalary: int = fmt"{salary}00".parseInt()
+    createAccount(username, fullname, passwd, formattedSalary)
 elif optionResult == 2:
-   #getAccount()
+    #getAccount()
+    discard
 elif optionResult == 3:
-   #cancelAccount()
+    #cancelAccount()
+    discard
 else:
-   echo "Please enter a valid option"
+    echo "Please enter a valid option"
