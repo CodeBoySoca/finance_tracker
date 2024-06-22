5aa4c558f4bdf00696e2251b2570b14a4952c29e
Rocky Latchman <kidmongoose@gmail.com>
2024-06-22 08:53:18 -0700

    Changed the formatting of the prompt to use stdout.write along with the echo function

--- src/finance_tracker.nim
+++ src/finance_tracker.nim
@@ -13,21 +13,27 @@ echo "---------------------------------------"
 echo "[1] create an account"
 echo "[2] sign into account"
 echo "[3] cancel your account"
-echo "Enter your choice and press Enter"
+echo "Enter your choice and press Enter "
 let optionResult = readLine(stdin).parseInt()
+echo "\n"
 
 if optionResult == 1:
-    echo "Very good! Lets set up your account\n"
-    echo "First, lets get your full name:"
+    echo "Very good! Lets set up your account"
+    echo "\n"
+    stdout.write("First, lets get your full name: ")
     let fullname: string = readLine(stdin)
-    echo "What is your username? (this is what you will use when you log in):"
+    echo "\n"
+    stdout.write("What is your username? (this is what you will use when you log in): ")
     let username: string = readLine(stdin)
-    echo "Nice, now we will need your password:"
+    echo "\n"
+    stdout.write("Nice, now we will need your password: ")
     let passwd: string = readLine(stdin)
+    echo "\n"
     echo "Next, to help you manage your money, we will need to know your monthly salary after taxes(ex. 5000)\n"
-    echo "salary:$"
+    stdout.write("salary:$ ")
     let salary: int = readLine(stdin).parseInt()
     let formattedSalary: int = fmt"{salary}00".parseInt()
+    echo "\n"
     createAccount(username, fullname, passwd, formattedSalary)
 elif optionResult == 2:
     #getAccount()
